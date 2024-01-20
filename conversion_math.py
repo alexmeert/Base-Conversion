1ee7d3e4bf388687e8110490cfbf0a49b4e0b9f4
Nicholas Alexander Meert <115733315+alexmeert@users.noreply.github.com>
2024-01-20 16:37:15 -0500

    Update conversion_math.py

--- conversion_math.py
+++ conversion_math.py
@@ -1,19 +1,103 @@
 # Decimal Conversion Functions
 def decimal_to_binary(num):
-    return bin(num)[2:]
+    # Split into integer and fractional parts
+    integer_part = int(num) # takes just the integer part of the number
+    fractional_part = num - integer_part # subtracts off the integer just leaving the decimal
+
+    # Convert integer part using the built-in bin function
+    binary_integer = bin(integer_part)[2:]
+
+    # Convert fractional part
+    binary_fraction = '.'
+    while fractional_part:
+        fractional_part *= 2
+        bit = int(fractional_part)
+        if bit == 1:
+            fractional_part -= bit
+        binary_fraction += str(bit)
+
+        # Limiting the length of the fractional part to avoid infinite loop
+        if len(binary_fraction) > 10:
+            break
+
+    return binary_integer + binary_fraction
+
 def decimal_to_octal(num):
-    return oct(num)[2:]
+    integer_part = int(num)
+    fractional_part = num - integer_part
+
+    # Convert integer part using the built-in oct function
+    octal_integer = oct(integer_part)[2:]
+
+    # Convert fractional part
+    octal_fraction = '.'
+    while fractional_part:
+        fractional_part *= 8
+        digit = int(fractional_part)
+        fractional_part -= digit
+        octal_fraction += str(digit)
+
+        # Limiting the length of the fractional part
+        if len(octal_fraction) > 10:
+            break
+
+    return octal_integer + octal_fraction
+
 def decimal_to_hex(num):
-    return hex(num)[2:].upper()
+    integer_part = int(num)
+    fractional_part = num - integer_part
+
+    # Convert integer part using the built-in hex function
+    hex_integer = hex(integer_part)[2:].upper()
+
+    # Convert fractional part
+    hex_fraction = '.'
+    while fractional_part:
+        fractional_part *= 16
+        digit = int(fractional_part)
+        fractional_part -= digit
+        hex_digit = hex(digit)[2:].upper()
+        hex_fraction += hex_digit
+
+        # Limiting the length of the fractional part
+        if len(hex_fraction) > 10:
+            break
+
+    return hex_integer + hex_fraction
 
 # Binary Conversion Functions
 def binary_to_decimal(binary_str):
-    return int(binary_str, 2)
+    if '.' in binary_str:
+        integer_part, fractional_part = binary_str.split('.')
+    else:
+        integer_part, fractional_part = binary_str, '0'
+
+    decimal_integer = int(integer_part, 2)
+    decimal_fraction = sum(int(bit) * 2 ** -(index + 1) for index, bit in enumerate(fractional_part))
+
+    return decimal_integer + decimal_fraction
 
 # Octal Conversion Functions
 def octal_to_decimal(octal_str):
-    return int(octal_str, 8)
+    if '.' in octal_str:
+        integer_part, fractional_part = octal_str.split('.')
+    else:
+        integer_part, fractional_part = octal_str, '0'
+
+    decimal_integer = int(integer_part, 8)
+    decimal_fraction = sum(int(digit) * 8 ** -(index + 1) for index, digit in enumerate(fractional_part))
+
+    return decimal_integer + decimal_fraction
+
 
 # Hex Conversion Functions
 def hex_to_decimal(hex_str):
-    return int(hex_str, 16)
+    if '.' in hex_str:
+        integer_part, fractional_part = hex_str.split('.')
+    else:
+        integer_part, fractional_part = hex_str, '0'
+
+    decimal_integer = int(integer_part, 16)
+    decimal_fraction = sum(int(digit, 16) * 16 ** -(index + 1) for index, digit in enumerate(fractional_part))
+
+    return decimal_integer + decimal_fraction
